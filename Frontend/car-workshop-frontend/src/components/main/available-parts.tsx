086ae2a020895391e66ccb6f4c2fc269331a148c
Bartosz Szczęsny <smphonebs@gmail.com>
2023-12-10 21:43:11 +0100

    Edit kontaktu i lokalizacji + Dodanie ofert

--- Frontend/car-workshop-frontend/src/components/main/available-parts.tsx
+++ Frontend/car-workshop-frontend/src/components/main/available-parts.tsx
@@ -1,6 +1,22 @@
+import React from "react";
 import warsztatImage from "../img/warsztatTło.png";
+import akebonoLogo from "../img/akebono.png";
+import bmwLogo from "../img/BMW.png";
+import boschLogo from "../img/bosch.png";
+import kybLogo from "../img/kyb.png";
+import maybachLogo from "../img/skoda.png";
+import rrLogo from "../img/mini.png";
 
 const AvailableParts = () => {
+  const availablePartsList = [
+    { brand: "Akebono", logo: akebonoLogo },
+    { brand: "BMW", logo: bmwLogo },
+    { brand: "Bosch", logo: boschLogo },
+    { brand: "KYB", logo: kybLogo },
+    { brand: "skoda", logo: maybachLogo },
+    { brand: "mini", logo: rrLogo },
+  ];
+
   return (
     <div
       className="w-full bg-no-repeat bg-cover bg-center"
@@ -10,10 +26,17 @@ const AvailableParts = () => {
       }}
       aria-label="Warsztat"
     >
-      <div className="h-16 md:h-24"></div>
-
       <div className="mx-auto max-w-screen-lg bg-black bg-opacity-80 text-white p-4 md:p-8 text-center">
-        <h2 className="text-3xl font-semibold">Kiedyś powstanie</h2>
+        <h2 className="text-3xl font-semibold mt-4">Dostępne Części w Warsztacie Samochodowym</h2>
+
+        <div className="flex flex-wrap justify-around items-center mt-4">
+          {availablePartsList.map((part, index) => (
+            <div key={index} className="m-4 w-1/3">
+              <img src={part.logo} alt={`${part.brand} logo`} className="h-32 w-full object-contain mb-2" />
+              <p>{part.brand}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- Frontend/car-workshop-frontend/src/components/main/contactAndLocation.tsx
+++ Frontend/car-workshop-frontend/src/components/main/contactAndLocation.tsx
@@ -1,9 +1,10 @@
+import React from "react";
 import warsztatImage from "../img/warsztatTło.png";
 
 const ContactLocation = () => {
   return (
     <div
-      className="w-full bg-no-repeat bg-cover bg-center"
+      className="w-full bg-no-repeat bg-cover bg-center relative"
       style={{
         backgroundImage: `url(${warsztatImage})`,
         minHeight: "100vh",
@@ -37,8 +38,25 @@ const ContactLocation = () => {
           <p>
             <span className="bg-black bg-opacity-80 px-2 py-1 rounded-md">
               Adres: ul. Armii Krajowej 78, 58-302 Wałbrzych
+              <br />
+              <br />
+              Trzeba chwile poczekać na api od Google-maps
             </span>
           </p>
+        </div>
+
+        {/* Dodaj odstęp po lokalizacji */}
+        <div className="mt-8"></div>
+
+        {/* Dostosuj element mapy do formy kwadratu */}
+        <div className="relative h-0 overflow-hidden" style={{ paddingBottom: "40%" }}>
+          <iframe
+            title="Mapa Google"
+            className="absolute top-0 left-0 w-full h-full"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2530.442021153748!2d16.280979315810552!3d50.75700977952712!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470fbc025010e2ab%3A0x676c0ef3980d5706!2sul.%20Armii%20Krajowej%2078%2C%2058-302%20Wa%C5%82brzych!5e0!3m2!1spl!2spl!4v1676006822277!5m2!1spl!2spl"
+            allowFullScreen
+            loading="lazy"
+          ></iframe>
         </div>
       </div>
     </div>
