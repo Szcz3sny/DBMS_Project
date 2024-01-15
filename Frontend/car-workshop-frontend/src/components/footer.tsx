a877556394a127ebf87f015b81e4b6593b8e71d5
Bartosz Szczęsny <smphonebs@gmail.com>
2024-01-15 08:13:23 +0100

    Wproawdzenie zmian i poprawek - NIE DO MODUŁÓW

--- Frontend/car-workshop-frontend/src/components/footer.tsx
+++ Frontend/car-workshop-frontend/src/components/footer.tsx
@@ -1,7 +1,8 @@
 export function Footer({}: React.HTMLAttributes<HTMLElement>) {
   return (
     <footer className="bg-black text-white py-4 text-center">
-      <div>&copy; 2023 Prawa zastrzeżone</div>
+      <div>&copy; 2024 Prawa zastrzeżone</div>
+      <div>Klaudia Melnyk, Krystian Flisak, Krzysztof Góra, Kamil Pawelski, Bartosz Szczęsny</div>
     </footer>
   );
 }
